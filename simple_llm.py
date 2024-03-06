054fd7a29ffe2655848642f0c3d64aa3589841fc
Meila Adisty Putri <160561377+MeilaAdisty@users.noreply.github.com>
2024-03-06 08:57:14 +0000

    Commit Keenam

---
+++ simple_llm.py
@@ -0,0 +1,15 @@
+import os
+from langchain_openai import ChatOpenAI
+import gradio as gr
+
+# Memasukkan API key
+os.environ["OPENAI_API_KEY"] = "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"
+
+gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )
+
+def chatbot(prompt):
+    return gpt3.invoke(prompt).content
+
+demo = gr.Interface(fn=chatbot, inputs="text", outputs="text")
+
+demo.launch(server_name="0.0.0.0", server_port= 7860, share=True)
